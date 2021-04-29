90161682379789d8d782dd028d6984877a353f14
Jeremy Roland <jeremy.roland@accenture.com>
2021-04-29 12:31:41 -0400

    bearscalesrcchanged

--- built/app.js
+++ built/app.js
@@ -55,7 +55,7 @@ class PuzzlePieces {
                 transform: {
                     local: {
                         position: { x: 0, y: -1, z: 0 },
-                        scale: { x: 0.001, y: 0.001, z: 0.001 }
+                        scale: { x: 1, y: 1, z: 1 }
                     }
                 }
             }
